9e3f869efa75a9269f4bdfb3f8aa0538865cb410
Cherry708 <aluser8241@ieselcaminas.org>
2021-09-30 19:34:04 +0200

    Finalizado

--- app/src/main/java/com/example/pmm_28poopropiedadesmetodosopcionalessetget_serranocanoruben_as/ProblemaPropuesto2.kt
+++ app/src/main/java/com/example/pmm_28poopropiedadesmetodosopcionalessetget_serranocanoruben_as/ProblemaPropuesto2.kt
@@ -2,10 +2,42 @@ package com.example.pmm_28poopropiedadesmetodosopcionalessetget_serranocanoruben
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pmm_28poopropiedadesmetodosopcionalessetget_serranocanoruben_as.databinding.ActivityProblemaPropuesto2Binding
 
 class ProblemaPropuesto2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_problema_propuesto2)
+
+        val binding = ActivityProblemaPropuesto2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnTirar30.setOnClickListener {
+            val dado = Dado(5)
+            dado.tirar()
+            binding.tvResult30.text = dado.toString()
+        }
+
+
     }
+}
+class Dado(valor:Int){
+    var valor = valor
+    set(value) {
+        if (value in 1..6){
+            field = value
+        } else
+            field = 1
+    }
+    get() = field
+
+    fun tirar(){
+        this.valor = (1..6).random()
+    }
+
+    override fun toString(): String {
+        return "Valor del dado: ${this.valor}"
+    }
+
+
 }
